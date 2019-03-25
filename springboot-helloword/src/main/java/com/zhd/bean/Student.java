b92d117cd7491e670a103515e3dbec63ce3d489b
TAIJI-zhanghd <srzm1213@163.com>
2019-03-25 19:41:12 +0800

    day04 homework

---
+++ springboot-helloword/src/main/java/com/zhd/bean/Student.java
@@ -0,0 +1,43 @@
+package com.zhd.bean;
+/**
+ * 使用bean一次性注入
+ */
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import lombok.ToString;
+
+
+@Component
+@Configuration
+@PropertySource(value = "classpath:test.properties")
+@ConfigurationProperties(prefix = "student")
+
+//@Data
+//@ToString
+public class Student {
+
+	private String name;
+	private Integer age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + "]";
+	}
+	
+	
+}
